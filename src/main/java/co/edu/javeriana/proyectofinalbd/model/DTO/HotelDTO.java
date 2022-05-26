f77e5d36c08f7ce0f53c75120db79ad5d343dc92
Betico1928 <betoveluis@gmail.com>
2022-05-26 14:54:58 -0500

    Create HotelDTO.java

---
+++ src/main/java/co/edu/javeriana/proyectofinalbd/model/DTO/HotelDTO.java
@@ -0,0 +1,24 @@
+package co.edu.javeriana.proyectofinalbd.model.DTO;
+
+public class HotelDTO
+{
+    private String nombre_hotel;
+
+    private String direccion_hotel;
+
+    private String telefono_hotel;
+
+    private int anio_construccion;
+
+    private String registro_operacion;
+
+    private int DNI;
+
+    private int Clasificacion_estrellas;
+
+    private String gerente;
+
+    private int departamentos;
+
+    private int fecha
+}
