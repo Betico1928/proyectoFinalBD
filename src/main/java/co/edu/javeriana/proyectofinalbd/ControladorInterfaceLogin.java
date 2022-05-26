55e5d5df0ace4e31eb32024c4b71dba440bf29ca
Betico1928 <betoveluis@gmail.com>
2022-05-26 14:45:45 -0500

    Metodos de Interfaz Hotel y RadioButtons

--- src/main/java/co/edu/javeriana/proyectofinalbd/ControladorInterfaceLogin.java
+++ src/main/java/co/edu/javeriana/proyectofinalbd/ControladorInterfaceLogin.java
@@ -10,30 +10,63 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 public class ControladorInterfaceLogin
 {
+    // Interfaz LogIn
     @FXML
     private Button botonConectarse;
-
     @FXML
     private Button botonRegistrarse;
-
     @FXML
     private Button botonLogIn;
-
     @FXML
     private TextField textUsuario;
-
     @FXML
     private TextField textContrasena;
-
     @FXML
     public Label textoProbarConexion;
+
+    // Interfaz Hotel
+    @FXML
+    private ToggleGroup clasificacion;
+    @FXML
+    private Button crearHotel;
+    @FXML
+    private RadioButton radio1;
+    @FXML
+    private RadioButton radio2;
+    @FXML
+    private RadioButton radio3;
+    @FXML
+    private RadioButton radio4;
+    @FXML
+    private RadioButton radio5;
+    @FXML
+    private TextField textoConsumo;
+    @FXML
+    private TextField textoDNIhotel;
+    @FXML
+    private TextField textoDepartamento;
+    @FXML
+    private TextField textoDireccion;
+    @FXML
+    private TextField textoHotelID;
+    @FXML
+    private TextField textoIVA;
+    @FXML
+    private TextField textoNombreHotel;
+    @FXML
+    private TextField textoPais;
+    @FXML
+    private TextField textoRegistroOperacion;
+    @FXML
+    private TextField textoTelefono;
+    @FXML
+    private TextField textoTurismo;
+
 
     // Para cambiar de Scene
     private Stage stage;
@@ -80,7 +113,32 @@ public class ControladorInterfaceLogin
         LogInDTO persona = uDAO.findByID(pLogIn);
     }
 
-
+    // Interfaz Hotel
+    @FXML
+    int seleccionarClasificacion(ActionEvent event)
+    {
+        int Seleccion = 0;
+        if (radio1.isSelected())
+        {
+            Seleccion = 1;
+        }
+        else if (radio2.isSelected())
+        {
+           Seleccion = 2;
+        } else if (radio3.isSelected())
+        {
+            Seleccion = 3;
+        }
+        else if (radio4.isSelected())
+        {
+            Seleccion = 4;
+        }
+        else if (radio5.isSelected())
+        {
+            Seleccion = 5;
+        }
+        return Seleccion;
+    }
 
 
     // Scene hotel
@@ -94,6 +152,4 @@ public class ControladorInterfaceLogin
     {
         FXMLLoader fxmlLoader = new FXMLLoader(mainApplication.class.getResource("interfazEmpleados.fxml"));
     }
-
-    //Scene
 }
