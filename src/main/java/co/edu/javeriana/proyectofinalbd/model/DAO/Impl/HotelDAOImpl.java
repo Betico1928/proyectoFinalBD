8243d757f8dc35685b6be467c518d29f5323d7e4
Betico1928 <betoveluis@gmail.com>
2022-05-26 15:31:55 -0500

    Update HotelDAOImpl.java

--- src/main/java/co/edu/javeriana/proyectofinalbd/model/DAO/Impl/HotelDAOImpl.java
+++ src/main/java/co/edu/javeriana/proyectofinalbd/model/DAO/Impl/HotelDAOImpl.java
@@ -5,12 +5,14 @@ import co.edu.javeriana.proyectofinalbd.model.DAO.HotelDAO;
 public class HotelDAOImpl implements HotelDAO
 {
     @Override
-    public HotelDAO create(HotelDAO hotelDAO) {
+    public HotelDAO create(HotelDAO hotelDAO)
+    {
         return null;
     }
 
     @Override
-    public HotelDAO buscarHotel(HotelDAO hotel) {
+    public HotelDAO buscarHotel(HotelDAO hotel)
+    {
         return null;
     }
 }
